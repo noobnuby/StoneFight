1d9e874b54725eaa55104a96d5fc4e8a3ae3837b
NOOBNUBY <noobnuby77@gmail.com>
2023-10-23 16:38:08 +0900

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,9 +44,8 @@ tasks {
         if(copy_dir != "") {
             doLast {
                 copy {
-                    val dir = File(copy_dir)
                     from(archiveFile)
-                    into(if (File(dir, archiveFileName.get()).exists()) dir else dir)
+                    into(if (File(copy_dir, archiveFileName.get()).exists()) copy_dir else copy_dir)
                 }
             }
         }
